078975878510868fe0c9a393f6e5e0c7406163f8
efryend <my.efryend@gmail.com>
2019-04-13 11:19:15 -0500

    Cambio 2

--- src/shared/activities/Home/Index.js
+++ src/shared/activities/Home/Index.js
@@ -15,7 +15,9 @@ class Home extends Component {
 
   constructor(props) {
     super(props)
-    
+
+
+
     
     this.fetchAmiiboStart     = this.fetchAmiiboStart.bind(this)
     this.fetchAmiiboElements  = this.fetchAmiiboElements.bind(this)
